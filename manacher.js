460a1b905ab7f2f4198cdab803ef8756f172ef8f
Larry Wu <lw7360@gmail.com>
2016-03-05 19:11:49 -0800

    Finish implementing manacher's algorithm

--- manacher.js
+++ manacher.js
@@ -1,4 +1,27 @@
 function manacher (str) {
+  var T = preProcess(str);
+  var P =  new Array(T.length).fill(0);
+  var C = 0;
+  var R = 0;
+
+  for (var i = 0; i < T.length - 1; i++) {
+    var mirr = 2 * C - i;
+
+    if (i < R) {
+      P[i] = Math.min(R - i, P[mirr]);
+    }
+
+    while (T[i + (1 + P[i])] == T[i - (1 + P[i])]) {
+      P[i]++;
+
+      if (i + P[i] > R) {
+        C = i;
+        R = i + P[i];
+      }
+    }
+  }
+
+  console.log(P);
 }
 
 function preProcess (str) {
